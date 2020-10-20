2a87dd0b5baeee433660ac3827b22c8e5d31629f
Яремко Роман <salvoronis@gmail.com>
2020-10-20 19:10:14 +0300

    allocate memory

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include "./flags.h"
 
 #define START 0xC5FA086B
@@ -13,64 +19,53 @@ const unsigned char afterfill = 	0x4;	//0000 0100
 const unsigned char dealloc = 		0x8;	//0000 1000
 
 void checkMem(char*);
-void *writeThread(void *argp);
-void writeRand(void);
-struct Vargp {
-	int from,to;
-};
+void write_to_memory(unsigned char * pointer);
 
 int main(int args, char * argv[]){
-	//check flags ./main -alloc=x, x={0,1,2,4,8}
+	//check flags
+	//example: ./main -alloc=x, x={0,1,2,4,8}
 	initFlag(args, argv);
 	int flags = 0; //standart flag
 	flag_int(&flags,"alloc");
 
 	//check do we need dump before allocate
-	if (flags & preallocate)
-		checkMem("./prealloc");
+	if (flags & preallocate){
+		puts("it's time to check memory(pre allocation)\npress enter to continue");
+		getchar();
+	}
+	
+	unsigned char* memory_pointer = (unsigned char*) START;
+	
+	/**There we allocate memory close to chosen memory
+	 * 242 Mb = 242*1024*1024
+	 * with permission to read and write
+	 * shared with other process and don't use file (anonimus)
+	 * because of anonimus file descriptor is -1
+	 * offset is 0 by the rules*/
+	memory_pointer = (unsigned char*)mmap(memory_pointer,242*1024*1024,PROT_READ|PROT_WRITE,MAP_SHARED|MAP_ANONYMOUS,-1,0);
+	//assert will output error if there is something wrong
+	assert(memory_pointer != MAP_FAILED);
+	/*printf("address -> %X\n",memory_pointer);
+	unsigned char test = 10;
+	memory_pointer[1] = test;*/
 
-	writeRand();
+	if (flags & afteralloc){
+		puts("it's time to check memorry(afret allocation)\npress enter to continue");
+		getchar();
+	}
+
+	write_to_memory(memory_pointer);
+
+	if (flags & afterfill) {
+		puts("it's time tocheck mamorry(after writting)\npress enter to continue");
+		getchar();
+	}
 
 	return 0;
 }
-
-//write random data to memory
-void writeRand(){
-	struct Vargp vars[35];
-	unsigned int written = START;
-	unsigned int to = written + BIT;
-	pthread_t tid;
-	for (int i = 0; i < 35; i++){
-		if ((END-to) < BIT)
-			to += END - to;
-		vars[i].from = written;
-		vars[i].to = to;
-		pthread_create(&tid, NULL, writeThread, &vars[i]);
-		written += BIT;
-		to += BIT;
-	}
-	pthread_exit(NULL);
-
-}
-
-
-void checkMem(char* filename){
-	unsigned int i = START;
-	unsigned int* symbol = i; //gets value by addser
-	FILE* fp;
-	if ((fp = fopen(filename,"wb")) == NULL){
-		perror("File opening error\n");
-		exit(1);
-	}
-	while(i <= END){
-		putc(symbol, fp); //put value in the file
-		i++;
-		symbol = i;
-	}
-}
-
-void *writeThread(void *vargp){
-	struct Vargp* variables = (struct Vargp*) vargp;
-	printf("from -> %x; to ->%x\n",variables->from, variables->to);
-	return NULL;
+/**Thit function accepts memory pointer
+ * start threads and make structures for thread's function
+ * 35 threads for 242mb which is
+ * 7 250 154 bytes for 1 thread and remainder 2 bytes*/
+void write_to_memory(unsigned char * memory_pointer){
 }
